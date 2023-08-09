b5548ff230a9916411ba59e6a5840f0fb01dfffb
Pedro Donato <emagdonatdonatoordep@gmail.com>
2023-08-09 09:13:00 -0300

    initial commit

---
+++ src/test/java/com/donatoordep/PersonServiceTest.java
@@ -0,0 +1,17 @@
+package com.donatoordep;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PersonServiceTest {
+
+    // Nomeclatura test[System Under Test]_[Condition or State Change]_[Expected Result]
+    @Test
+    @DisplayName("Display name")
+    // Altera o nome do teste para maior legibilidade
+    void testABCD_When_XYZ_ShouldReturn_FSAD() {
+        // Given / Arrange - Cenário inicial das classes (setar configurações, iniciar variaveis)
+        // When / Act- Inicia a execução do cenário.
+        // Then / Assert - Avaliação do resultado, verifica se corresponde ao esperado.
+    }
+}
